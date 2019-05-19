36d1b91dc0da11ef525ed60acdf42b8be89be9a3
Арсен Татраев <ArsT-98@mail.ru>
2019-05-19 19:45:12 +0300

    Добавлены класс и файл ГРАФИКА

---
+++ Schedule/Schedule/Models/Timetable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedule.Models
+{
+    public class Specialty
+    {
+        public string SpecialtyName { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+
+    public class Course
+    {
+        public string CourseNumber { get; set; }
+        public List<Day> Days { get; set; }
+    }
+
+    public class Day
+    {
+        public int ThisDate { get; set; }
+        public int ThisMonth { get; set; }
+        public string Content { get; set; }
+    }
+}
