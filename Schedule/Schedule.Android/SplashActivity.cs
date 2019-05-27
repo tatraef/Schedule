6ff09d8db8fd45a74d9e3a7a0888239b3f400bc6
Арсен Татраев <ArsT-98@mail.ru>
2019-05-27 18:50:04 +0300

    Добавлена заставка при загрузке приложения на Android.

---
+++ Schedule/Schedule.Android/SplashActivity.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using Schedule.Droid;
+
+namespace com.xamarin.sample.splashscreen
+{
+    [Activity(Label = "Schedule", Icon = "@mipmap/icon", Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
