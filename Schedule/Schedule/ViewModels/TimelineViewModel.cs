080e98d69c4c1ac42b2166dc91165e26486c30a1
Арсен Татраев <ArsT-98@mail.ru>
2019-07-22 12:12:37 +0300

    Добавлена проверка на актуальность расписания рейтингов и экзаменов для студента.

--- Schedule/Schedule/ViewModels/TimelineViewModel.cs
+++ Schedule/Schedule/ViewModels/TimelineViewModel.cs
@@ -328,7 +328,7 @@ namespace Schedule.ViewModels
                             case "Э": LoadExamsForStudent(couples, NeedDate); break;
                             case "Г": NothingInteresting(couples, ""); break;
                             case "У": NothingInteresting(couples, "П"); break;
-                            case "К": NothingInteresting(couples, ""); break;
+                            case "К": NothingInteresting(couples, "К"); break;
                             case "П": NothingInteresting(couples, "П"); break;
                             case "Н": NothingInteresting(couples, "Н"); break;
                             default:
@@ -369,13 +369,15 @@ namespace Schedule.ViewModels
             if (s == "П")
                 some.CoupleName = "Практика...";
             else if (s == "К")
-                some.CoupleName = "Каникулы";
+                some.CoupleName = "Каникулы!";
             else if (s == "Д")
-                some.CoupleName = "Дипломная работа на носу...";
+                some.CoupleName = "Диплом...";
             else if (s == "Н")
                 some.CoupleName = "Научно-исследовательская работа...";
             else if (s == "Э")
                 some.CoupleName = "Экзамены да зачеты...";
+            else if (s == "Р")
+                some.CoupleName = "Рейтинги...";
             else
                 some.CoupleName = "Ничего интересного...";
 
@@ -385,64 +387,82 @@ namespace Schedule.ViewModels
         public void LoadRaitingForStudent(List<Couple> couples, DateTime NeedDate)
         {
             Couple someCouple;
-            //проверяется факультет
-            if (App.Current.Properties.TryGetValue("facultyName", out object FacultyName))
+
+            //Определение, когда было загружено расписание рейтингов
+            DateTime lastUpdate = DateTime.Parse((string) App.Current.Properties["updateRait"]);
+            TimeSpan difference = NeedDate.Subtract(lastUpdate);
+            int days = difference.Days;
+            //если более чем 30 дней назад, то значит нового расписания еще нет,
+            //показать в таймлайне просто "Рейтинги"
+            if (Math.Abs(days) > 30)
             {
-                DateTime now = DateTime.Now;
-                string dt = NeedDate.DayOfWeek.ToString().ToLower();
-
-                string facultyName = (string)FacultyName;
-                //проверяются номер группы, имя группы и подгруппа
-                string groupId = "";
-                if (App.Current.Properties.TryGetValue("groupId", out object GroupId))
-                { groupId = (string)GroupId; }
-                string groupName = "";
-                if (App.Current.Properties.TryGetValue("groupName", out object GroupName))
-                { groupName = (string)GroupName; }
-                string subgroup = null;
-                if (App.Current.Properties.TryGetValue("subgroup", out object Subgroup))
-                { subgroup = (string)Subgroup; }
-
-                foreach (var f in App.facultiesRait)
+                NothingInteresting(couples, "Р");
+            }
+            else
+            {
+                //проверяется факультет
+                if (App.Current.Properties.TryGetValue("facultyName", out object FacultyName))
                 {
-                    if (f.FacultyName == facultyName)
-                    {
-                        foreach (var g in f.Groups)
-                        {
-                            if (g.GroupId == groupId && g.GroupName == groupName)
-                            {
-                                for (int i = 0; i < g.Couples.Count; i++)
-                                {
-                                    if (g.Couples[i].Day == dt && g.Couples[i].Week == "1" && g.Couples[i].SubgroupName == subgroup)
-                                    {
-                                        //проверка, чтобы не показывать уже завершенные пары
-                                        if (NeedDate.Day == now.Day)
-                                        {
-                                            string[] s = g.Couples[i].TimeEnd.Split(':');
-                                            int h = Convert.ToInt32(s[0]);
-                                            int m = Convert.ToInt32(s[1]);
-                                            TimeSpan t = new TimeSpan(h, m, 0);
-                                            if (t < now.TimeOfDay)
-                                            {
-                                                continue;
-                                            }
-                                        }
-                                        someCouple = new Couple(g.Couples[i]);
-                                        //проверка на два рейтинга за пару
-                                        if (i + 1 < g.Couples.Count && g.Couples[i + 1].Week == "2" && g.Couples[i+1].SubgroupName == subgroup && g.Couples[i + 1].CoupleName != someCouple.CoupleName)
-                                        {
-                                            someCouple.CoupleName += ", " + g.Couples[i + 1].CoupleName;
-                                        }
-                                        else if (i + 2 < g.Couples.Count && g.Couples[i + 2].Week == "2" && g.Couples[i + 2].CoupleName != someCouple.CoupleName)
-                                        {
-                                            someCouple.CoupleName += ", " + g.Couples[i + 2].CoupleName;
-                                        }
+                    DateTime now = DateTime.Now;
+                    string dt = NeedDate.DayOfWeek.ToString().ToLower();
 
-                                        couples.Add(someCouple);
+                    string facultyName = (string)FacultyName;
+                    //проверяются номер группы, имя группы и подгруппа
+                    string groupId = "";
+                    if (App.Current.Properties.TryGetValue("groupId", out object GroupId))
+                    { groupId = (string)GroupId; }
+                    string groupName = "";
+                    if (App.Current.Properties.TryGetValue("groupName", out object GroupName))
+                    { groupName = (string)GroupName; }
+                    string subgroup = null;
+                    if (App.Current.Properties.TryGetValue("subgroup", out object Subgroup))
+                    { subgroup = (string)Subgroup; }
+
+                    foreach (var f in App.facultiesRait)
+                    {
+                        if (f.FacultyName == facultyName)
+                        {
+                            foreach (var g in f.Groups)
+                            {
+                                if (g.GroupId == groupId && g.GroupName == groupName)
+                                {
+                                    for (int i = 0; i < g.Couples.Count; i++)
+                                    {
+                                        if (g.Couples[i].Day == dt && g.Couples[i].Week == "1" && g.Couples[i].SubgroupName == subgroup)
+                                        {
+                                            //проверка, чтобы не показывать уже завершенные пары
+                                            if (NeedDate.Day == now.Day)
+                                            {
+                                                string[] s = g.Couples[i].TimeEnd.Split(':');
+                                                int h = Convert.ToInt32(s[0]);
+                                                int m = Convert.ToInt32(s[1]);
+                                                TimeSpan t = new TimeSpan(h, m, 0);
+                                                if (t < now.TimeOfDay)
+                                                {
+                                                    continue;
+                                                }
+                                            }
+                                            someCouple = new Couple(g.Couples[i]);
+                                            //проверка на два рейтинга за пару
+                                            if (i + 1 < g.Couples.Count && g.Couples[i + 1].Week == "2" && g.Couples[i + 1].SubgroupName == subgroup && g.Couples[i + 1].CoupleName != someCouple.CoupleName)
+                                            {
+                                                someCouple.CoupleName += ", " + g.Couples[i + 1].CoupleName;
+                                            }
+                                            else if (i + 2 < g.Couples.Count && g.Couples[i + 2].Week == "2" && g.Couples[i + 2].CoupleName != someCouple.CoupleName)
+                                            {
+                                                someCouple.CoupleName += ", " + g.Couples[i + 2].CoupleName;
+                                            }
+
+                                            couples.Add(someCouple);
+                                        }
                                     }
                                 }
                             }
                         }
+                    }
+                    if (couples.Count == 0)
+                    {
+                        NothingInteresting(couples, "");
                     }
                 }
             }
@@ -452,7 +472,6 @@ namespace Schedule.ViewModels
         {
             DateTime now = DateTime.Now;
             Dictionary<byte, TeacherCouple> teacherCouplesRaiting = new Dictionary<byte, TeacherCouple>();
-            List<TeacherCouple> teacherCoupleListRaiting = new List<TeacherCouple>();
 
             string dt = NeedDate.DayOfWeek.ToString().ToLower();
 
@@ -587,59 +606,70 @@ namespace Schedule.ViewModels
         public void LoadExamsForStudent(List<Couple> couples, DateTime NeedDate)
         {
             Couple someCouple;
-            //проверяется факультет
-            if (App.Current.Properties.TryGetValue("facultyName", out object FacultyName))
-            {
-                string facultyName = (string)FacultyName;
-                //проверяются номер группы, имя группы и подгруппа
-                string groupId = "";
-                if (App.Current.Properties.TryGetValue("groupId", out object GroupId))
-                { groupId = (string)GroupId; }
-                string groupName = "";
-                if (App.Current.Properties.TryGetValue("groupName", out object GroupName))
-                { groupName = (string)GroupName; }
-                string subgroup = null;
-                if (App.Current.Properties.TryGetValue("subgroup", out object Subgroup))
-                { subgroup = (string)Subgroup; }
 
-                foreach (var f in App.facultiesExam)
+            //Определение, когда было загружено расписание экзаменов
+            DateTime lastUpdate = DateTime.Parse((string)App.Current.Properties["updateExam"]);
+            TimeSpan difference = NeedDate.Subtract(lastUpdate);
+            int days = difference.Days;
+            //если более чем 30 дней назад, то значит нового расписания еще нет,
+            //показать в таймлайне просто "Экзамены"
+            if (Math.Abs(days) > 30)
+            {
+                NothingInteresting(couples, "Э");
+            }
+            else
+            {
+                //проверяется факультет
+                if (App.Current.Properties.TryGetValue("facultyName", out object FacultyName))
                 {
-                    if (f.FacultyName == facultyName)
+                    string facultyName = (string)FacultyName;
+                    //проверяются номер группы, имя группы и подгруппа
+                    string groupId = "";
+                    if (App.Current.Properties.TryGetValue("groupId", out object GroupId))
+                    { groupId = (string)GroupId; }
+                    string groupName = "";
+                    if (App.Current.Properties.TryGetValue("groupName", out object GroupName))
+                    { groupName = (string)GroupName; }
+
+                    foreach (var f in App.facultiesExam)
                     {
-                        foreach (var g in f.Groups)
+                        if (f.FacultyName == facultyName)
                         {
-                            if (groupName.Contains(g.GroupName) && g.Course == groupId[0].ToString())
+                            foreach (var g in f.Groups)
                             {
-                                for (int i = 0; i < g.Couples.Count; i++)
+                                if (groupName.Contains(g.GroupName) && g.Course == groupId[0].ToString())
                                 {
-                                    if (Convert.ToDateTime(g.Couples[i].Day).Day == NeedDate.Day)
+                                    for (int i = 0; i < g.Couples.Count; i++)
                                     {
-                                        someCouple = new Couple(g.Couples[i]);
-                                        bool added = false;
-                                        for (int j = 0; j < couples.Count; j++)
+                                        if (Convert.ToDateTime(g.Couples[i].Day).Day == NeedDate.Day)
                                         {
-                                            if (Convert.ToDateTime(couples[j].TimeBegin) > Convert.ToDateTime(someCouple.TimeBegin))
+                                            someCouple = new Couple(g.Couples[i]);
+                                            bool added = false;
+                                            for (int j = 0; j < couples.Count; j++)
                                             {
-                                                couples.Insert(j, someCouple);
-                                                added = true;
-                                                break;
+                                                if (Convert.ToDateTime(couples[j].TimeBegin) > Convert.ToDateTime(someCouple.TimeBegin))
+                                                {
+                                                    couples.Insert(j, someCouple);
+                                                    added = true;
+                                                    break;
+                                                }
+                                            }
+                                            if (!added)
+                                            {
+                                                couples.Add(someCouple);
                                             }
                                         }
-                                        if (!added)
-                                        {
-                                            couples.Add(someCouple);
-                                        }  
                                     }
+                                    break;
                                 }
-                                break;
                             }
                         }
                     }
-                }
 
-                if (couples.Count == 0)
-                {
-                    NothingInteresting(couples, "");
+                    if (couples.Count == 0)
+                    {
+                        NothingInteresting(couples, "");
+                    }
                 }
             }
         }
