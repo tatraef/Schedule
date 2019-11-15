9b498fc86ed998c0507cd6e316e7c0f4420f9b47
Arsen <Tatraevarsen98@gmail.com>
2019-11-15 18:36:19 +0300

    Удалил код с загрузкой рейтингов, более не актуально.

--- Schedule/Schedule/ViewModels/TimelineViewModel.cs
+++ Schedule/Schedule/ViewModels/TimelineViewModel.cs
@@ -187,8 +187,6 @@ namespace Schedule.ViewModels
             {
                 SortedDictionary<byte, TeacherCouple> sortedTeacherCouples = new SortedDictionary<byte, TeacherCouple>(teacherCouples);
 
-                LoadRaitingForTeacher(sortedTeacherCouples, NeedDate);
-
                 teacherCoupleList = sortedTeacherCouples.Values.ToList();
 
                 LoadExamsForTeacher(teacherCoupleList, NeedDate);
@@ -292,7 +290,6 @@ namespace Schedule.ViewModels
         public void WhatTodayForStudent(List<Couple> couples, DateTime NeedDate)
         {
             //Что сегодня? Обычный/Выходной/Практика/Экзамен/Рейтинг?
-            int firstWeekOfSecondSemestr = 0;
 
             int day = NeedDate.Day;
             int month = NeedDate.Month;
@@ -316,30 +313,16 @@ namespace Schedule.ViewModels
                                 break;
                         }
                     }
+                    else
+                    {
+                        break;
+                    }
 
                     if (couples.Count > 0)
                     {
                         break;
                     }
 
-                    //если через шесть дней сессия или если сейчас девятая неделя семестра, то это рейтинг
-                    //Что если за шесть дней до сесии были выходные, тогда определение не правильное? Нужно доработать. change
-                    if (i + 6 < App.myTimetable.Count && (App.myTimetable[i + 6].Content == "Э" || App.myTimetable[i].ThisWeek - firstWeekOfSecondSemestr == 9))
-                    {
-                        LoadRaitingForStudent(couples, NeedDate);
-                        if (couples.Count == 0)
-                        {
-                            NothingInteresting(couples, "");
-                        }
-                        break;
-                    }
-
-                }
-                //Определение первой недели второго семестра для определения первого рейтинга второго семестра, 
-                //ведь он начинаяется на девятой неделе после каникул
-                if (App.myTimetable[i].Content == "К")
-                {
-                    firstWeekOfSecondSemestr = App.myTimetable[i].ThisWeek;
                 }
             }
         }
@@ -363,225 +346,6 @@ namespace Schedule.ViewModels
                 some.CoupleName = "Ничего интересного...";
 
             couples.Add(some);
-        }
-
-        public void LoadRaitingForStudent(List<Couple> couples, DateTime NeedDate)
-        {
-            Couple someCouple;
-
-            //Определение, когда было загружено расписание рейтингов
-            DateTime lastUpdate = DateTime.Parse((string) App.Current.Properties["updateRait"]);
-            TimeSpan difference = NeedDate.Subtract(lastUpdate);
-            int days = difference.Days;
-            //если более чем 30 дней назад, то значит нового расписания еще нет,
-            //показать в таймлайне просто "Рейтинги"
-            if (Math.Abs(days) > 30)
-            {
-                NothingInteresting(couples, "Р");
-            }
-            else
-            {
-                //проверяется факультет
-                if (App.Current.Properties.TryGetValue("facultyName", out object FacultyName))
-                {
-                    DateTime now = DateTime.Now;
-                    string dt = NeedDate.DayOfWeek.ToString().ToLower();
-
-                    string facultyName = (string)FacultyName;
-                    //проверяются номер группы, имя группы и подгруппа
-                    string groupId = "";
-                    if (App.Current.Properties.TryGetValue("groupId", out object GroupId))
-                    { groupId = (string)GroupId; }
-                    string groupName = "";
-                    if (App.Current.Properties.TryGetValue("groupName", out object GroupName))
-                    { groupName = (string)GroupName; }
-                    string subgroup = null;
-                    if (App.Current.Properties.TryGetValue("subgroup", out object Subgroup))
-                    { subgroup = (string)Subgroup; }
-
-                    foreach (var f in App.facultiesRait)
-                    {
-                        if (f.FacultyName == facultyName)
-                        {
-                            foreach (var g in f.Groups)
-                            {
-                                if (g.GroupId == groupId && g.GroupName == groupName)
-                                {
-                                    for (int i = 0; i < g.Couples.Count; i++)
-                                    {
-                                        if (g.Couples[i].Day == dt && g.Couples[i].Week == "1" && g.Couples[i].SubgroupName == subgroup)
-                                        {
-                                            //проверка, чтобы не показывать уже завершенные пары
-                                            if (NeedDate.Day == now.Day)
-                                            {
-                                                string[] s = g.Couples[i].TimeEnd.Split(':');
-                                                int h = Convert.ToInt32(s[0]);
-                                                int m = Convert.ToInt32(s[1]);
-                                                TimeSpan t = new TimeSpan(h, m, 0);
-                                                if (t < now.TimeOfDay)
-                                                {
-                                                    continue;
-                                                }
-                                            }
-                                            someCouple = new Couple(g.Couples[i]);
-                                            //проверка на два рейтинга за пару
-                                            if (i + 1 < g.Couples.Count && g.Couples[i + 1].Week == "2" && g.Couples[i + 1].SubgroupName == subgroup && g.Couples[i + 1].CoupleName != someCouple.CoupleName)
-                                            {
-                                                someCouple.CoupleName += ", " + g.Couples[i + 1].CoupleName;
-                                            }
-                                            else if (i + 2 < g.Couples.Count && g.Couples[i + 2].Week == "2" && g.Couples[i + 2].CoupleName != someCouple.CoupleName)
-                                            {
-                                                someCouple.CoupleName += ", " + g.Couples[i + 2].CoupleName;
-                                            }
-
-                                            couples.Add(someCouple);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    if (couples.Count == 0)
-                    {
-                        NothingInteresting(couples, "");
-                    }
-                }
-            }
-        }
-
-        public void LoadRaitingForTeacher(SortedDictionary<byte, TeacherCouple> sortedTeacherCouples, DateTime NeedDate)
-        {
-            DateTime now = DateTime.Now;
-            Dictionary<byte, TeacherCouple> teacherCouplesRaiting = new Dictionary<byte, TeacherCouple>();
-
-            string dt = NeedDate.DayOfWeek.ToString().ToLower();
-
-            //проверяется имя преподавателя
-            if (App.Current.Properties.TryGetValue("teacherName", out object AppTeacherName))
-            {
-                string thisTeacher = (string)AppTeacherName;
-                foreach (var f in App.facultiesRait)
-                {
-                    foreach (var g in f.Groups)
-                    {
-                        for (int j = 0; j < g.Couples.Count; j++)
-                        {
-                            //Contains, так как в паре английского может быть несколь преподавателей
-                            if (g.Couples[j].CoupleTeacher.Contains(thisTeacher))
-                            {
-                                if (g.Couples[j].Week == "1")
-                                {
-                                    if (g.Couples[j].Day == dt)
-                                    {
-                                        //проверка, чтобы не показывать уже завершенные пары
-                                        if (NeedDate.Day == now.Day)
-                                        {
-                                            string[] s = g.Couples[j].TimeEnd.Split(':');
-                                            int h = Convert.ToInt32(s[0]);
-                                            int m = Convert.ToInt32(s[1]);
-                                            TimeSpan t = new TimeSpan(h, m, 0);
-                                            if (t < now.TimeOfDay)
-                                            {
-                                                continue;
-                                            }
-                                        }
-                                        #region Проверка в графике, учится ли данная группа, если да, то пара добавляется в коллекцию
-                                        int indexOfDigit = 0;
-                                        for (int i = 0; i < g.GroupName.Length; i++)//вырезаем код специальности, чтобы по нему искать
-                                        {
-                                            if (Char.IsDigit(g.GroupName[i]))
-                                            {
-                                                indexOfDigit = i;
-                                                break;
-                                            }
-                                        }
-
-                                        string code = g.GroupName.Substring(indexOfDigit, 8);
-                                        string course = g.GroupId[0].ToString();
-
-                                        foreach (var item in App.timetable)
-                                        {
-                                            if (item.SpecialtyName.Contains(code))
-                                            {
-                                                foreach (var courses in item.Courses)
-                                                {
-                                                    if (courses.CourseNumber == course)
-                                                    {
-                                                        int day = NeedDate.Day;
-                                                        int month = NeedDate.Month;
-                                                        for (int i = 0; i < courses.Days.Count; i++)
-                                                        {
-                                                            if (courses.Days[i].ThisDay == day && courses.Days[i].ThisMonth == month)
-                                                            {
-                                                                if (i + 6 < courses.Days.Count && (courses.Days[i + 6].Content == "Э" || courses.Days[i].ThisWeek - 24 == 9)) //change 24
-                                                                {
-                                                                    if (courses.Days[i].Content == null)
-                                                                    {
-                                                                        byte coupleNum = Convert.ToByte(g.Couples[j].CoupleNum);
-
-                                                                        if (!teacherCouplesRaiting.ContainsKey(coupleNum))
-                                                                        {
-                                                                            teacherCouplesRaiting.Add(coupleNum, new TeacherCouple(g.Couples[j],
-                                                                                g.GroupId + (g.Couples[j].SubgroupId != null ? "(" + g.Couples[j].SubgroupId + ")" : "")));
-                                                                        }
-                                                                        else
-                                                                        {
-                                                                            teacherCouplesRaiting[coupleNum].CoupleTeacher += ", " + g.GroupId + (g.Couples[j].SubgroupId != null ? "(" + g.Couples[j].SubgroupId + ")" : "");
-                                                                        }
-
-                                                                        //проверка на два рейтинга за пару
-                                                                        if (j + 1 < g.Couples.Count && g.Couples[j + 1].SubgroupId == g.Couples[j].SubgroupId && g.Couples[j + 1].CoupleName != teacherCouplesRaiting[coupleNum].CoupleName)
-                                                                        {
-                                                                            teacherCouplesRaiting[coupleNum].CoupleName += ", " + g.Couples[j + 1].CoupleName;
-                                                                        }
-                                                                        else if (j + 2 < g.Couples.Count && g.Couples[j + 2].SubgroupId != null && g.Couples[j + 2].CoupleName != teacherCouplesRaiting[coupleNum].CoupleName)
-                                                                        {
-                                                                            //рейтинг уже может быть записан после первой группы, поэтому проверка
-                                                                            if (!teacherCouplesRaiting[coupleNum].CoupleName.Contains(g.Couples[j + 2].CoupleName))
-                                                                            {
-                                                                                teacherCouplesRaiting[coupleNum].CoupleName += ", " + g.Couples[j + 2].CoupleName;
-                                                                            }
-                                                                        }
-                                                                    }
-                                                                    break;
-                                                                }
-                                                            }
-                                                        }
-                                                        break;
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        #endregion  
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (teacherCouplesRaiting.Count > 0)
-            {
-                //Соединение рейтингов с парами
-                if (teacherCouplesRaiting != null)
-                {
-                    foreach (var item in sortedTeacherCouples)
-                    {
-                        if (!teacherCouplesRaiting.ContainsKey(item.Key))
-                        {
-                            teacherCouplesRaiting.Add(item.Key, item.Value);
-                        }
-                    }
-
-                    SortedDictionary<byte, TeacherCouple> sortedTeacherCouplesRaiting = new SortedDictionary<byte, TeacherCouple>(teacherCouplesRaiting);
-                    sortedTeacherCouples.Clear();
-                    foreach (KeyValuePair<byte, TeacherCouple> item in sortedTeacherCouplesRaiting)
-                    {
-                        sortedTeacherCouples.Add(item.Key, item.Value);
-                    }
-                }
-            }
         }
 
         public void LoadExamsForStudent(List<Couple> couples, DateTime NeedDate)
