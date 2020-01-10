4d107e008ff9502cc90d7569b00d7e25c320dbae
Arsen <Tatraevarsen98@gmail.com>
2020-01-10 17:44:50 +0300

    Страница "Обратная связь".

--- Schedule/Schedule/Views/ContactUsPage.xaml.cs
+++ Schedule/Schedule/Views/ContactUsPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace Schedule.Views
 {
@@ -15,6 +16,42 @@ namespace Schedule.Views
         public ContactUsPage()
         {
             InitializeComponent();
+        }
+
+        private async void FeedbackButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameEntry.Text) || 
+                string.IsNullOrEmpty(SubjectEntry.Text) || 
+                string.IsNullOrEmpty(MessageEditor.Text))
+            {
+                await DisplayAlert("Ошибка", "Все поля обязательны для заполнения!", "OK");
+            }
+            else
+            {
+                await SendEmail(SubjectEntry.Text, MessageEditor.Text + "/n/n" + NameEntry.Text, new List<string> { "sschedule@inbox.ru" });
+            }
+        }
+
+        public async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
+            }
         }
     }
 }
